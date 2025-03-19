86acb9c3a92921ddce5e12de9117c8126eb77b22
Egezenn <egezenn@tutanota.com>
2025-03-19 20:32:38 +0300

    init

---
+++ main.py
@@ -0,0 +1,156 @@
+import html
+import json
+import re
+import sys
+
+import svg
+
+elements = []
+key_dimension = 100
+
+if len(sys.argv) > 2:
+    layout = sys.argv[-3]
+    script = sys.argv[-2]
+    output = sys.argv[-1]
+else:
+    print("No arguments given, provide layout, script and output name.")
+    exit()
+
+# select based on input
+with open(f"layouts/{layout}.json", "r", encoding="utf-8") as file:
+    layout_data = json.load(file)
+
+try:
+    with open(script, "r", encoding="utf-8") as script:
+        data = script.read()
+        matches = re.findall(r"^\s*?[^\s*?;](.*?)::", data, re.MULTILINE)
+        sanitized_matches = []
+        for match in matches:
+            sanitized_matches.append(
+                match.lower()
+                .strip()
+                .replace("\t", "")
+                .replace("~", "")
+                .replace("$", "")
+                .replace("<", "")
+                .replace(">", "")
+                .replace("#", "")
+                .replace("^", "")
+                .replace("!", "")
+                .replace("+", "")
+            )
+        keybinds = []
+        keybinds = [
+            x for x in sanitized_matches if not (x in keybinds or keybinds.append(x))
+        ]
+
+        # make subset layouts keys with modifiers
+except:
+    print("Script file not found!")
+
+
+def return_with_modifiers_preset(bg_fill="white"):
+    global x_0, y_0, column, key_dimension, modified_height, modified_width
+    return [
+        svg.Rect(
+            x=x_0,
+            y=y_0,
+            width=key_dimension * modified_width,
+            height=key_dimension * modified_height,
+            stroke="black",
+            fill=bg_fill,
+            stroke_width=2,
+        ),
+        svg.Text(
+            x=x_0 + (key_dimension / 2) * modified_width,
+            y=y_0 + key_dimension / 2,
+            text=html.escape(column).replace("&lt;br&gt;", ""),
+            fill="black",
+            font_size=16,
+            font_weight="bold",
+            font_family="Calibri",
+            text_anchor="middle",
+        ),
+    ]
+
+
+def return_normal_preset(bg_fill="white"):
+    global x_0, y_0, column, key_dimension, modified_height, modified_width
+    return [
+        svg.Rect(
+            x=x_0,
+            y=y_0,
+            width=key_dimension,
+            height=key_dimension,
+            stroke="black",
+            fill=bg_fill,
+            stroke_width=2,
+        ),
+        svg.Text(
+            x=x_0 + key_dimension / 2,
+            y=y_0 + key_dimension / 2,
+            text=html.escape(column).replace("&lt;br&gt;", ""),
+            fill="black",
+            font_size=16,
+            font_weight="bold",
+            font_family="Calibri",
+            text_anchor="middle",
+        ),
+    ]
+
+
+modified_width = 1
+modified_height = 1
+y_0 = 0
+for rows in layout_data:
+    x_0 = 0
+    for column in rows:
+        # each key is iterated, modifiers (width, height modifications skips to the next iteration)
+        if type(column) == dict:
+            if "x" in column:  # spacing
+                x_0 += key_dimension * float(column["x"])
+            # "big ass enter", without the width modification (for now)
+            if "h" and "h2" in column:
+                modified_height = float(column["h"]) + float(column["h2"])
+            if "w" in column:  # width
+                modified_width = float(column["w"])
+                continue
+            if "h" in column:  # height
+                modified_height = float(column["h"])
+                continue
+
+        else:
+            if column.lower() in keybinds:
+                if modified_width != 1 or modified_height != 1:
+                    elements.append(return_with_modifiers_preset(bg_fill="red"))
+                    x_0 += key_dimension * modified_width
+                    modified_width = 1
+                    modified_height = 1
+                else:
+                    elements.append(return_normal_preset(bg_fill="red"))
+                    x_0 += key_dimension
+            else:
+                if modified_width != 1 or modified_height != 1:
+                    elements.append(return_with_modifiers_preset())
+                    x_0 += key_dimension * modified_width
+                    modified_width = 1
+                    modified_height = 1
+                else:
+                    elements.append(return_normal_preset())
+                    x_0 += key_dimension
+    y_0 += key_dimension
+
+# x_0 += key_dimension  # because last column isn't a starting point
+
+canvas = svg.SVG(
+    width=x_0,
+    height=y_0,
+    elements=[
+        svg.Rect(width=x_0, height=y_0, stroke="lightgray", fill="lightgray"),
+        *elements,
+    ],
+)
+
+
+with open(output, "w", encoding="utf-8") as svg:
+    svg.write(str(canvas))
---
+++ readme.md
@@ -0,0 +1,23 @@
+# AutoHotkey Layout Visualizer
+
+Visualize your keybinds!
+
+![example](assets/example.png)
+
+## Usage
+
+`python main.py ISO105_tr-TR script.ahk out.svg`
+
+## Information
+
+[Layouts](/layouts/) are pulled from [here](https://raw.githubusercontent.com/ijprest/keyboard-layout-editor/refs/heads/master/layouts.json).
+
+References [AutoHotkey key list](https://www.autohotkey.com/docs/v1/KeyList.htm) from the docs. Scan codes (SCxxx) and virtual keys (VKxxx) are ignored.
+
+(only [tr-TR](layouts/ISO105_tr-TR.json) is in the proper format, do a PR if you make one for yours).
+
+For other keyboard globalizations you can look at [Microsoft's learn pages](https://learn.microsoft.com/en-us/globalization/windows-keyboard-layouts) regarding the topic.
+
+## Dependencies
+
+[svg.py](https://github.com/orsinium-labs/svg.py), Licensed under MIT license.
